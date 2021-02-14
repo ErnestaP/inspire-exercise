7b93fe3e08b8f6772214945d3eaa57939463e779
ErnestaP <ernesta.petraityte@yahoo.com>
2021-02-14 13:02:20 +0100

    list visualized by using antd list

--- src/components/listItemView.tsx
+++ src/components/listItemView.tsx
@@ -1,20 +1,27 @@
 import * as React from 'react'
-import { getListItemsFromData } from '../getters/getListItemsFromData'
+import { List, Avatar, Button, Skeleton } from 'antd';
 
+import { getListItemsFromData } from '../getters/getListItemsFromData'
 import { useGetData } from '../hooks/useGetData'
-import { ListItem } from './litsItem'
 
 interface ListItemView {
-    listItems: any
+  listItems: any
 }
 
 export const ListItemView = () => {
-    const { data } = useGetData('https://inspirehep.net/api/literature')
-    const list = getListItemsFromData(data)
+  const { data } = useGetData('https://inspirehep.net/api/literature')
+  const list = getListItemsFromData(data)
 
-    return <div>
-        {list.map(item => (
-            <ListItem item={item} />
-        ))}
-    </div>
+  return <List
+    itemLayout="horizontal"
+    dataSource={list}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          title={item.title}
+          description={item.abstracts}
+        />
+      </List.Item>
+    )}
+  />
 }

a3ed41d6192607682045c2f23b1b7503ece83dc4
ErnestaP <ernesta.petraityte@yahoo.com>
2021-02-14 15:44:11 +0100

    added interface

--- src/hooks/useGetData.tsx
+++ src/hooks/useGetData.tsx
@@ -23,7 +23,7 @@ export interface ListItemPRops {
 }
 
 export const useGetData = (url: string) => {
-  const [data, setData] = useState<ListDataProps | any>()
+  const [data, setData] = useState<ListDataProps | ListItemPRops>()
   const [loader, setLoader] = useState(true)
 
   useEffect(() => {
